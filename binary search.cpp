67b304e638891186cd108b4602d60eda1ca77c26
Yash Mina <yashkhodia01@gmail.com>
2022-02-11 01:56:28 +0530

    Binary Search

---
+++ binary search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[9]={1,4,7,9,11,12,14,17,19};
+    int l=0,h=8,mid,key;
+
+    cout<<"Enter Key "<<endl;
+    cin>>key;
+
+    while(l<=h)
+    {
+        mid=(l+h)/2;
+        {
+            if(key==A[mid])
+          { cout<<"Found At "<<mid<<endl;
+           return 0;}
+           
+            else if (key<A[mid])
+            h=mid-1;
+
+            else l=mid+1;
+         }
+        }
+        cout<<"Not Found "<<endl;
+        return 0;
+
+    }
