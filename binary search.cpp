5cea57d9164458336073eae28fccb879eca190ef
Yash Mina <yashkhodia01@gmail.com>
2022-03-08 03:18:35 +0530

    Optimized

--- binary search.cpp
+++ binary search.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int binarySearch(int arr[], int n, int key){
     int low=0,high=n-1,mid;
     while(low<=high){
-        mid = (low + high)/2;
+
+        mid = low + (high - low)/2;
+
         if(key == arr[mid])
         return mid;
         else if(key > arr[mid])
