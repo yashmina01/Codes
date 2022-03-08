4c04f5f65a20d375cf20612525158f3f8d100540
Yash Mina <yashkhodia01@gmail.com>
2022-03-09 03:59:43 +0530

    Time-16 ms(43.62%) Space - 11.5 MB(89.99%)

--- Peak Index in a Mountain Array.cpp
+++ Peak Index in a Mountain Array.cpp
@@ -10,8 +10,7 @@ public:
             
             if(arr[mid] < arr [mid+1]){
                 start = mid + 1;
-            }
-            
+            }     
             else end = mid;
         }
         return start;
