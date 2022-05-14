8302659056f5dde487fe2dfef32d7a537069bb8d
Yash Mina <yashkhodia01@gmail.com>
2022-05-15 02:52:19 +0530

    Palindrom With Extra Space

---
+++ Codes/PalindromeString.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s; cin>>s;
+
+    int start = 0,end = s.size()-1;
+    while(start <= end){
+        if(s[start] != s[end]){
+        cout<<"Not Palindrome"<<endl;
+        return 0;}
+
+        else {
+            start++;
+            end--;
+        }
+    }
+    cout<<"Palindrome String"<<endl;
+}
