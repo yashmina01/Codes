bae99392d925db5a42c8bf7e39be81f2b16ff431
Yash Mina <yashkhodia01@gmail.com>
2022-06-03 13:33:02 +0530

    Notes On npos string function

---
+++ Codes/stingnpos.cpp
@@ -0,0 +1,6 @@
+    if(temp.find(s2) != string::npos)
+    return true;
+
+    //Here npos represent as no position , (-1) , False ,Here we trying to find s2 in temp string ,so if that != npos,
+    //that means our s2 is present in temp string ;
+    //Alse npos check the s2 till the end of the string temp; 
